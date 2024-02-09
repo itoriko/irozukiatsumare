303f68a2a9b0ac99000aca276666868d0afc4fb0
itoriko <127394855+itoriko@users.noreply.github.com>
2024-02-09 10:53:43 +0000

    ログイン機能途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ユーザ用
+  # URL /users/sign_in ...
+  devise_for :users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    root to: "homes#top"
+  end
+
+  scope module: :public do
+    root to: "homes#top"
+  end
 end
