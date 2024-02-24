47b57881c79836c7ac825a9ac3e4d2b30ae79a5e
itoriko <127394855+itoriko@users.noreply.github.com>
2024-02-24 04:41:29 +0000

    rack_profile修正

--- config/initializers/rack_profile.rb
+++ config/initializers/rack_profile.rb
@@ -1,4 +1,3 @@
-# rack-mini-profiler（画面の左上に出てくる〇〇ms）の非表示
-if Rails.env.development?
-  Rack::MiniProfiler.config.start_hidden = false
+if defined?(Rack::MiniProfiler)
+  Rack::MiniProfiler.config.auto_inject = false
 end
